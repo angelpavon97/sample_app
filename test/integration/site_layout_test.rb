87133ad318ca6eecb35ef8cf1b3345918bb12e9b
Angel <angelpavonperez@gmail.com>
2019-02-09 12:10:09 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,8 +9,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
 
   end
+  
+  test "layout links logged in" do
+    @user = users(:michael)
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+
+  end
+  
 end
